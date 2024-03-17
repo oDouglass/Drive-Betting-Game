ed401e9419f4e28add9d2f5d047d8e94011e9966
oDouglass <douglasluiz_855@hotmail.com>
2024-03-17 18:53:50 -0300

    V 2. 0
    V 2.0

--- bet.js
+++ bet.js
@@ -1,44 +1,128 @@
 let saldo = 100;
+let raceInterval;
+let pilotoVencedor;
+let apostaInput;
+let pilotoInput;
+let aposta;
+let escolhaPiloto;
 
+window.onload = iniciarJogo;
+
+// Initial function of the game
 function iniciarJogo() {
     updateSaldo();
+    resetCarro();
 }
 
+// Betting conference function
 function realizarAposta() {
-    let apostaInput = document.getElementById('aposta');
-    let pilotoInput = document.getElementById('piloto');
+    apostaInput = document.getElementById('aposta');
+    pilotoInput = document.getElementById('piloto');
 
-    let aposta = parseFloat(apostaInput.value);
-    let escolhaPiloto = parseInt(pilotoInput.value);
+    aposta = parseFloat(apostaInput.value);
+    escolhaPiloto = parseInt(pilotoInput.value);
 
     if (isNaN(aposta) || aposta < 5 || aposta > saldo || isNaN(escolhaPiloto) || escolhaPiloto < 1 || escolhaPiloto > 5) {
         alert("Por favor, insira valores válidos.");
+        clearInterval(raceInterval);
         return;
     }
+    travarCampo();
+    raceInterval = setInterval(moveCars, 50);
+}
 
-    let pilotoVencedor = Math.floor(Math.random() * 5) + 1;
+// Function Moving Cars
+function moveCars() {
+    let cars = document.querySelectorAll('.car');
 
-    console.log("Piloto Vencedor:", pilotoVencedor);
+    cars.forEach(car => {
+        if (parseInt(car.style.left) < 980) {
+            let distance = Math.floor(Math.random() * 10) + 1;
+            car.style.left = (parseInt(car.style.left) || 0) + distance + 'px';
+        }
 
+        if (parseInt(car.style.left) >= 980) {
+            clearInterval(raceInterval);
+            pilotoVencedor = car.id;
+            verificarVencedor();
+        }
+    });
+}
+
+// Function that verifies the winner
+function verificarVencedor() {
+
+    switch(pilotoVencedor) {
+        case "car1":
+            pilotoVencedor = 1;
+            alert("Pilot N° 1 WON");
+          break;
+        case "car2":
+            pilotoVencedor = 2;
+            alert("Pilot N° 2 WON");
+          break;
+        case "car3":
+            pilotoVencedor = 3;
+            alert("Pilot N° 3 WON");
+          break;
+        case "car4":
+            pilotoVencedor = 4;
+            alert("Pilot N° 4 WON");
+          break;
+        case "car5":
+            pilotoVencedor = 5;
+            alert("Pilot N° 5 WON");
+          break;
+    }
 
     if (escolhaPiloto === pilotoVencedor) {
         let ganho = aposta * 2;
         saldo += ganho;
-        exibirResultado(`Parabéns! Seu piloto venceu. Você ganhou R$${ganho}.`);
+        exibirResultado(`Congratulations! Its pilot won. You've earned R$${ganho}.`);
     } else {
         saldo -= aposta;
-        exibirResultado(`Infelizmente, seu piloto não venceu. Você perdeu R$${aposta}.`);
+        exibirResultado(`Unfortunately, his driver didn't win. You've lost R$${aposta}.`);
     }
 
     updateSaldo();
+    resetCarro();
+    liberarCampo();
 }
 
+// Function that places the cars at the start of the race
+function resetCarro() {
+    let cars = document.querySelectorAll('.car');
+    let atraso = 500;
+    cars.forEach((car, index) => {
+        setTimeout(() => { 
+            car.style.left = '0px';
+    }, index * atraso);
+    });
+}
+
+// Function to update the player's balance
 function updateSaldo() {
-    document.getElementById('saldo').textContent = `Your Current Balance: R$${saldo}`;
+    document.getElementById('saldo').textContent = `Current Balance: R$${saldo}`;
 }
 
+// Display the result in HTML
 function exibirResultado(mensagem) {
-    document.getElementById('resultado').textContent = mensagem;
+    document.getElementById('result').textContent = mensagem;
 }
 
-window.onload = iniciarJogo;
+// Text field lock function
+function travarCampo() {
+    var campo1 = document.getElementById("aposta");
+    var campo2 = document.getElementById("piloto");
+    campo1.setAttribute("readonly", "true");
+    campo2.setAttribute("readonly", "true");
+}
+
+// Function of releasing the text field
+function liberarCampo() {
+    var campo1 = document.getElementById("aposta");
+    var campo2 = document.getElementById("piloto");
+    campo1.removeAttribute("readonly");
+    campo2.removeAttribute("readonly");
+}
+
